5ce81af76ca889417a8498ef310a03897a870c79
ltixier2 <40599718+ltixier2@users.noreply.github.com>
2022-03-13 17:41:35 +0100

    Create vaultCorrigé.sol

---
+++ 5. Smart contract Solidity & bonnes pratiques de sécurité/vaultCorrigé.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.12;
+ 
+// You can store ETH in this contract and redeem them.
+contract Vault {
+    mapping(address => uint) public balances;
+ 
+    /// @dev Store ETH in the contract.
+    function store() public payable {
+        balances[msg.sender]+=msg.value;
+    }
+    
+    /// @dev Redeem your ETH.
+    function redeem() public {
+        uint valueToSend = balances[msg.sender];
+        balances[msg.sender]=0;
+        msg.sender.call{ value: valueToSend }("");
+
+    }
+}
