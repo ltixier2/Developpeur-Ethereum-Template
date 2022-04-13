e01be98fe085fd3ede4b0fdddb7e908d69879b04
ltixier2 <40599718+ltixier2@users.noreply.github.com>
2022-04-13 22:40:20 +0200

    Update audit1OnlyComments.sol

--- 5. Smart contract Solidity & bonnes pratiques de sécurité/audit1OnlyComments.sol
+++ 5. Smart contract Solidity & bonnes pratiques de sécurité/audit1OnlyComments.sol
@@ -2,23 +2,29 @@ pragma solidity ^0.5.12;
  // force usage of a single version of compiler and have the most recent one. 
 
 contract Crowdsale {
-   using SafeMath for uint256;
- 
+   //using SafeMath for uint256;
+  // useless
    address public owner; // the owner of the contract
-   address public escrow; // wallet to collect raised ETH
+   address payable public  escrow; // wallet to collect raised ETH added payable.
    uint256 public savedBalance = 0; // Total amount raised in ETH
    mapping (address => uint256) public balances; // Balances in incoming Ether
  
    // Initialization
-   function Crowdsale(address _escrow) {
-       owner = tx.origin;
-       // a require that uses onlyOwner replace this. 
+   
+   
+   constructor  Crowdsale(address _escrow) {
+       //replaced function by constructor
+       owner = msg.sender;
+       //tx.origin should be replaced by msg.sender 
+
        // add address of the specific contract
        escrow = _escrow;
    }
   
    // function to receive ETH
-   function() public {
+    receive()  payable external {
+       // set the function payable to recieve ethers
+       
        balances[msg.sender] += msg.value;
        savedBalance += msg.value;
        escrow.send(msg.value);
@@ -28,11 +34,12 @@ contract Crowdsale {
    function withdrawPayments() public{
        address payee = msg.sender;
        uint256 payment = balances[payee];
- 
+       balances[payee] = 0;
        payee.send(payment);
  
        savedBalance -= payment;
-       balances[payee] = 0;
-       // to be sure that reentrency is not used set balance at the top of the function 
+       
+       // need to be sure that reentrency is not used set balance at the top of the function set the balance at 0 before sending payments
+       
    }
 }
